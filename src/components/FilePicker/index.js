78e6b587e452a70f3a306bd237c3e0a3086ecec1
charles0818 <charles081889@gmail.com>
2020-05-13 18:40:38 +0100

    initialized Audio recording Permisssion

--- src/components/FilePicker/index.js
+++ src/components/FilePicker/index.js
@@ -2,15 +2,14 @@ import * as React from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
+import { Audio } from 'expo-av';
 
-export const usePermission  = (type = 'image') => {
+export const usePermission  = (type = 'camera') => {
   const [image, setImage] = React.useState(null);
   const detectPermission = React.useCallback((type) => {
     switch(type) {
-      case 'image':
+      case 'camera':
         return Permissions.CAMERA, Permissions.CAMERA_ROLL
-      case 'recording':
-        return Permissions.AUDIO_RECORDING
       case 'location':
         return Permissions.LOCATION
       default:
@@ -22,7 +21,7 @@ export const usePermission  = (type = 'image') => {
       if (Constants.platform.ios) {
         const { status } = await Permissions.askAsync(detectPermission(type));
         if (status !== 'granted') {
-          alert('Sorry, we need camera roll permissions to make this work!');
+          alert(`Sorry, we need ${type} permissions to make this work!`);
         }
       }
     };
@@ -62,3 +61,55 @@ export const usePermission  = (type = 'image') => {
   };
   return { pickImage, image, setImage, accessCamera }
 }
+
+export const useMediaPermissons = () => {
+  const Recording = async () => {
+    const [recordingStatus, setRecordingStatus] = React.useState({
+      canRecord: false, isDoneRecording: false, durationMillis: 0, URI: ''
+    });
+    React.useEffect(() => {
+      const getPermissionAsync = async () => {
+        const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+        if (status !== 'granted') {
+          alert(`Sorry, we need recording permissions to make this work!`);
+        }
+      };
+      getPermissionAsync();
+    }, [])
+    const recording = new Audio.Recording();
+    try {
+      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.startAsync();
+      recording.setOnRecordingStatusUpdate(status => setRecordingStatus(status))
+      recording.setProgressUpdateInterval(1000);
+      // You are now recording!
+    } catch (error) {
+      // An error occurred!
+      console.log(error);
+      alert(`An error occured while trying to intitiate recording. Kindly try again.`);
+    }
+    const stopRecording = async () => {
+      try {
+        const status = await recording.stopAndUnloadAsync();
+        const URI = await recording.getURI()
+        const { sound, status: playbackStatus } = await recording.createNewLoadedSoundAsync(
+          {
+            progressUpdateIntervalMillis: 500,
+            positionMillis: 0,
+            shouldPlay: false,
+            rate: 1.0,
+            shouldCorrectPitch: false,
+            volume: 1.0,
+            isMuted: false,
+            isLooping: false,
+          },
+        );
+        setRecordingStatus({...status, URI, sound});
+      } catch (error) {
+        
+      }
+    }
+    return { recordingStatus, stopRecording, recording }
+  }
+  return { Recording,  }
+};
